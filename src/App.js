d789b4d88fafe5a2d8b98a9d53bad6b5ba40d5dd
ILyaZabaluev <ilya.zabaluev.89@mail.ru>
2024-04-28 13:50:02 +0300

    task 9

--- src/App.js
+++ src/App.js
@@ -4,25 +4,25 @@ import { TimerBlock } from './components/timerBlock';
 import { createElement } from 'react';
 
 export const App = () => {
-	return (
-		//  Императивный
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				{/* Декларативный */}
-				<TimerBlock />
-			</header>
-		</div>
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement('img', { className: 'App-logo', src: logo, alt: 'logo' }),
+			createElement('p', {}, 'Edit <code>src/App.js</code> and save to reload.'),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			<TimerBlock />,
+		),
 	);
 };
